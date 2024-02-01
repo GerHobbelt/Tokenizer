1072e245ae7aaf1938365b0827cceaa7c6264394
Jungi KIM <jungi.kim@systrangroup.com>
2024-01-31 19:50:41 -0800

    CI: install into /install

--- bindings/python/tools/prepare_build_environment_windows.sh
+++ bindings/python/tools/prepare_build_environment_windows.sh
@@ -14,9 +14,9 @@ rm -r *_Release
 rm -rf build
 mkdir build
 cd build
-cmake -DLIB_ONLY=ON -DICU_ROOT=$ROOT_DIR/icu/ -DCMAKE_INSTALL_PREFIX=$TOKENIZER_ROOT ..
+cmake -DLIB_ONLY=ON -DICU_ROOT=$ROOT_DIR/icu/ -DCMAKE_INSTALL_PREFIX=$TOKENIZER_ROOT/install ..
 cmake --build . --config Release --target install
 
 cp $ROOT_DIR/icu/bin64/icudt*.dll $ROOT_DIR/bindings/python/pyonmttok/
 cp $ROOT_DIR/icu/bin64/icuuc*.dll $ROOT_DIR/bindings/python/pyonmttok/
-cp $TOKENIZER_ROOT/bin/OpenNMTTokenizer.dll $ROOT_DIR/bindings/python/pyonmttok/
+cp $TOKENIZER_ROOT/install/bin/OpenNMTTokenizer.dll $ROOT_DIR/bindings/python/pyonmttok/
