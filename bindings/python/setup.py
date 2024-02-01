b1e7e21c8ebcf7acbaf105a877128217b2cedd1e
Jungi KIM <jungi.kim@systrangroup.com>
2024-02-01 14:17:45 -0800

    put back debug output

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -37,15 +37,16 @@ def _maybe_add_library_root(lib_name, header_only=False):
             if os.path.isdir(lib_dir):
                 library_dirs.append(lib_dir)
                 break
+    print("include_dirs: " + str(include_dirs))
 
 
 def _maybe_add_dic_resources():
     jiebadic_src = os.environ.get("CPPJIEBA_DIC")
     mecabdic_src = os.environ.get("MECAB_DIC")
     mecabkodic_src = os.environ.get("MECABKO_DIC")
-    # print(f"CPPJIEBA_DIC: {jiebadic_src}")
-    # print(f"MECAB_DIC: {mecabdic_src}")
-    # print(f"MECABKO_DIC: {mecabkodic_src}")
+    print("CPPJIEBA_DIC: " + str(jiebadic_src))
+    print("MECAB_DIC: " + str(mecabdic_src))
+    print("MECABKO_DIC: " + str(mecabkodic_src))
     base_dir = os.path.dirname(os.path.abspath(__file__))
     jiebadic_tgt = os.path.join(base_dir, "pyonmttok", "cppjieba_dic")
     mecabdic_tgt = os.path.join(base_dir, "pyonmttok", "mecab_dic")
@@ -71,7 +72,7 @@ def _maybe_add_dic_resources():
             pass
         package_data["pyonmttok.mecabko_dic"] = ["*.bin", "*.def", "*.dic", "dicrc"]
 
-    # print(f"{package_data = }")
+    print("package_data: " + str(package_data))
 
 
 _maybe_add_library_root("TOKENIZER")
